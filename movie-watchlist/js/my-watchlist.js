2162116611f380e64e20557541c91d841093fe70
Martin Lorimer <martin.lorimer@icloud.com>
2022-05-16 16:04:07 +0200

    Add remove watchlist movies functionality and add placeholder text/styling

--- movie-watchlist/js/my-watchlist.js
+++ movie-watchlist/js/my-watchlist.js
@@ -22,39 +22,29 @@ window.onload = function() {
 function fetchMovieList(id) {
     fetch(`http://www.omdbapi.com/?apikey=c59f01a8&s=${id}`)
     .then(resolve => resolve.json() )
-    .then(data => {
+    .then( data => {
         renderMovie(data.Search, containerMovies)
     })
 }
 
 function renderMovie(data, container) {
     container.innerHTML = ""
-    if (data === undefined) {
-        container.innerHTML = `
-        <div class="container-placeholder">
-            <p class="placeholder-text">
-            Unable to find what you’re looking for. 
-            Please try another search
-            
-            </p>
-        </div>`
-    }
     for (let movie of data) {
         fetch(`http://www.omdbapi.com/?apikey=c59f01a8&i=${movie.imdbID}`)
         .then(resolve => resolve.json() )
         .then( movie => {
             container.innerHTML += `
             <div class="container-movie">
-                <div id="column-one" class="column-one">
-                    <img src="${movie.Poster}" alt="poster of the movie">
-                </div>
-                <div id="column-two" class="column-two">
-                    <h2 id="title">${movie.Title}<span class="ratings">⭐${movie.imdbRating}</span></h2>
-                    <span class="runtime movie-info">${movie.Runtime}</span>
-                    <span class="genre movie-info">${movie.Genre}</span>
-                    <span data-imdbID="${movie.imdbID}" class="watchlist movie-info">➕ Watchlist</span>
-                    <p id="movie-plot">${movie.Plot}</p>
-                </div>
+            <div id="column-one" class="column-one">
+            <img src="${movie.Poster}" alt="poster of the movie">
+            </div>
+            <div id="column-two" class="column-two">
+            <h2 id="title">${movie.Title}<span class="ratings">⭐${movie.imdbRating}</span></h2>
+            <span class="runtime movie-info">${movie.Runtime}</span>
+            <span class="genre movie-info">${movie.Genre}</span>
+            <span data-imdbID="${movie.imdbID}" class="watchlist movie-info">➖ Watchlist</span>
+            <p id="movie-plot">${movie.Plot}</p>
+            </div>
             </div>
             `
         })
@@ -67,8 +57,10 @@ movieForm.addEventListener("submit", event => {
     fetchMovieList(ourFormData)
 })
 
-containerMovies.addEventListener('click', function(e){
+containerWatchlist.addEventListener('click', function(e){
     watchlistArray = JSON.parse(localStorage.getItem("array:"))
-    watchlistArray.push({"imdbID": e.target.dataset.imdbid})
+    const itemToRemove = watchlistArray.findIndex(num => num.imdbID === e.target.dataset.imdbid)
+    watchlistArray.splice(itemToRemove, 1)
     localStorage.setItem("array:", JSON.stringify(watchlistArray))
+    renderMovie(watchlistArray, containerWatchlist)
  })
