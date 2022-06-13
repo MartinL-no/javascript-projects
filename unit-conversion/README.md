3ac6fd34ff6d6af28dddda74496839441411efcf
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-06-13 09:34:40 +0200

    Fixed bugs

--- unit-conversion/README.md
+++ unit-conversion/README.md
@@ -2,7 +2,22 @@ Unit Conversion Webapp Project
 
 Objectives
 - Recreate webapp based on prototype given as Figma design
-- Sav number to a variable in code
+- Save number to a variable in code
 - Round numbers to 3 decimal places
 - Give top of page CSS gradient 
 - BONUS TASK - Add ability for user input to change the number and automatically recalculate values when it changes
+
+Things I Learnt From This Project
+- Adding gradients in CSS
+- Matching layout to Figma prototpye
+- Adding external fonts
+- How to round up numbers in Javascript
+- Styling input fields and buttons
+- Styling tricks like autoselecting field on loadup and removing arrows from edge of input box
+- Extracting values/numbers from input fields and feeding them into Javascript functions
+- What event triggers are and how to add/use them with input fields
+- How to work out problems myself and find awnswers through the internet
+
+Hardest Parts Of This Project
+- Working out how to bring field inputs into function after page load
+- Troubleshooting why I was getting NaN in HTML output after first calculation
--- unit-conversion/js/main.js
+++ unit-conversion/js/main.js
@@ -1,30 +1,25 @@
-let input = document.getElementById("input").innerText
+// Initilize input variable
+input = document.getElementById("input")
 
+// Initilize measurement variables
 let lengthText = document.getElementById("length")
 let volumeText = document.getElementById("volume")
 let massText = document.getElementById("mass")
 
-// let poundsToKilograms = input * 0.453592.toFixed(3)
-// let kilogramsToPounds = input * 2.20462.toFixed(3)
-
-function printLength() {
+function calculate() {
+// Convert input variable to value inputted by user
+    input = input.value
+// Convert measurement variables to converted value based on user input
     let feetCalc = input * 3.28084
     let metersCalc = input * 0.3048
-    lengthText.innerText = input + " meters = " + feetCalc.toFixed(3) + " feet | " + input + " feet = " + metersCalc.toFixed(3) + " meters"
-}
-
-function printVolume() {
     let litersCalc = input * 3.78541
     let gallonsCalc = input * 0.264172
-    volumeText.innerText = input + " liters = " + gallonsCalc.toFixed(3) + " gallons | " + input + " gallons = " + litersCalc.toFixed(3) + " liters"
-}
-
-function printMass() {
     let kilogramsCalc = input * 0.453592
     let poundsCalc = input * 2.20462
+// Print calculations and text to DOM
+    lengthText.innerText = input + " meters = " + feetCalc.toFixed(3) + " feet | " + input + " feet = " + metersCalc.toFixed(3) + " meters"
+    volumeText.innerText = input + " liters = " + gallonsCalc.toFixed(3) + " gallons | " + input + " gallons = " + litersCalc.toFixed(3) + " liters"
     massText.innerText = input + " kilos = " + poundsCalc.toFixed(3) + " pounds | " + input + " pounds = " + kilogramsCalc.toFixed(3) + " kilos"
+// Reset input to initial value in preperation for next calculation
+    input = document.getElementById("input")
 }
-
-printLength()
-printVolume()
-printMass()
